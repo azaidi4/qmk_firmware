9e199c98b03720c64e1b145aa7ff5227e45277ab
Ahmad Zaidi <ahmad_zafar@outlook.com>
2022-08-09 05:14:01 -0500

    update ikki68 aurora

--- keyboards/wuque/ikki68/config.h
+++ keyboards/wuque/ikki68/config.h
@@ -26,10 +26,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROW_PINS { B0, B1, B2, B3, E6 }
 #define MATRIX_COL_PINS { D1, D0, D2, D3, D5, D4, D6, D7, B4, B5, F0, F1, B6, F4, F5, F6,F7 }
 
-#define DIODE_DIRECTION COL2ROW 
+#define DIODE_DIRECTION COL2ROW
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
+
+/* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
+ * This is userful for the Windows task manager shortcut (ctrl+shift+esc).
+ */
+#define GRAVE_ESC_CTRL_OVERRIDE
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
